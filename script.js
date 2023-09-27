46c5b19ddd54ed9a1a4dade616cf36afce2a93af
MikkelViggo <mikkelviggo@gmail.com>
2023-09-27 12:49:27 +0200

    script-inputs

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dataForm = document.getElementById('data-form');
+    const nameInput = document.getElementById('name');
+    const weightInput = document.getElementById('weight');
+    const amountInput = document.getElementById('amount');
+    const searchInput = document.getElementById('search-bar');
+    const resultsList = document.getElementById('results');
+
+});
