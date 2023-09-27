c8f59951f2d14b99c1b861625c1cd6170eadfbda
MikkelViggo <mikkelviggo@gmail.com>
2023-09-27 12:54:14 +0200

    script-filter-data

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('search-bar');
     const resultsList = document.getElementById('results');
 
+    //Load dataen fra databasen
+    let database = [];
+    function updateResults(query) {
+        resultsList.innerHTML = '';
+
+        const filteredData = database.filter(item =>
+            item.name.toLowerCase().includes(query.toLowerCase()),
+            item.weight.toLowerCase().includes(query.toLowerCase()),
+            item.amount.toLowerCase().includes(query.toLowerCase())
+        );
+        
+        filteredData.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `Name: ${item.name}, Weight: ${item.weight}, Amount: ${item.amount}`;
+            resultsList.appendChild(listItem);
+        });
+    }
 });
